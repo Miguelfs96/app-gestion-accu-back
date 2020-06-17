507146e3c1f61182ced89fa756e50b236bbd83cc
Miguel <fernandez.s.miguel96@gmail.com>
2020-06-17 17:10:29 +0200

    Commit inicial

---
+++ src/main/java/org/accu/gestion/gestionback/GestionBackApplication.java
@@ -0,0 +1,13 @@
+package org.accu.gestion.gestionback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/accu/gestion/gestionback/GestionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.accu.gestion.gestionback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
